5e437fe713889834b79f11098a49fa489e409174
TheCoder <31217771+SakthiKishore@users.noreply.github.com>
2021-01-13 15:21:27 +0530

    Update json_DataFrame.py

--- Miscellaneous/json_DataFrame.py
+++ Miscellaneous/json_DataFrame.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 # update path of json file
 
-df = pd.read_json (r'C:\Users\Ron\Desktop\data.json')
+df = pd.read_json (r'C:\Users\SakthiKishore\Desktop\data.json')
 
 print (df)
