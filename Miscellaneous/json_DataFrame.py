4008f114a727e4793c3774cc07568b0613324614
TheCoder <31217771+SakthiKishore@users.noreply.github.com>
2021-01-13 15:25:41 +0530

    Update json_DataFrame.py

--- Miscellaneous/json_DataFrame.py
+++ Miscellaneous/json_DataFrame.py
@@ -6,4 +6,7 @@ df = pd.read_json (r'C:\Users\SakthiKishore\Desktop\data.json')
 
 #pd.read_csv for csv file
 
-print (df)
+# print (df)
+# df.to_csv - for write to csv file
+
+df.to_excel("json_output.xlsx")  
