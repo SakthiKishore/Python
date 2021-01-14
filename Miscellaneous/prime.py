6b7854d82b92b1458956e25e727f333d372efc26
TheCoder <31217771+SakthiKishore@users.noreply.github.com>
2021-01-14 18:29:27 +0530

    Update prime.py

--- Miscellaneous/prime.py
+++ Miscellaneous/prime.py
@@ -5,6 +5,5 @@ def is_prime(num):
     for n in range(2,num):
         if num % n == 0:
             print(num,'is not prime')
-            break
-    else: # If never mod zero, then prime
-        print(num,'is prime!')
+        else: # If never mod zero, then prime
+            print(num,'is prime!')
